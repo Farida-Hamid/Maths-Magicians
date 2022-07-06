ca37f524790040266cac51d6a3016c2a9572bd31
Farida-Hamid <faridaaddala@gmail.com>
2022-07-06 11:53:43 +0300

    Display butons in the right order

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,14 +1,5 @@
 import React from 'react';
-// class buttons extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//   render() {
-//     return (
-//     <div>this.value</div>
-//     );
-//   }
-// };
+
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
   // constructor(props) {
@@ -18,24 +9,34 @@ class Calculator extends React.Component {
     return (
       <div>
         <div className="screen" />
-        <button type="button" className="gray">AC</button>
-        <button type="button" className="gray">+/-</button>
-        <button type="button" className="gray">%</button>
-        <button type="button" className="orange">&#247;</button>
-        <button type="button" className="gray">7</button>
-        <button type="button" className="gray">8</button>
-        <button type="button" className="gray">9</button>
-        <button type="button" className="orange">X</button>
-        <button type="button" className="gray">4</button>
-        <button type="button" className="gray">5</button>
-        <button type="button" className="gray">6</button>
-        <button type="button" className="orange">-</button>
-        <button type="button" className="gray">1</button>
-        <button type="button" className="gray">2</button>
-        <button type="button" className="gray">3</button>
-        <button type="button" className="orange">+</button>
-        <button type="button" className="zero">0</button>
-        <button type="button" className="orange">=</button>
+        <div className="row">
+          <button type="button" className="gray">AC</button>
+          <button type="button" className="gray">+/-</button>
+          <button type="button" className="gray">%</button>
+          <button type="button" className="orange">&#247;</button>
+        </div>
+        <div className="row">
+          <button type="button" className="gray">7</button>
+          <button type="button" className="gray">8</button>
+          <button type="button" className="gray">9</button>
+          <button type="button" className="orange">x</button>
+        </div>
+        <div className="row">
+          <button type="button" className="gray">4</button>
+          <button type="button" className="gray">5</button>
+          <button type="button" className="gray">6</button>
+          <button type="button" className="orange">-</button>
+        </div>
+        <div className="row">
+          <button type="button" className="gray">1</button>
+          <button type="button" className="gray">2</button>
+          <button type="button" className="gray">3</button>
+          <button type="button" className="orange">+</button>
+        </div>
+        <div className="row">
+          <button type="button" className="zero">0</button>
+          <button type="button" className="orange">=</button>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// eslint-disable-next-line react/prefer-stateless-function
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
