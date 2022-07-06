494ac1ca7fe059cd3e1660c882b187f373357365
Farida-Hamid <faridaaddala@gmail.com>
2022-07-06 18:41:16 +0300

    Fix linter error

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -12,51 +12,50 @@ class Calculator extends React.PureComponent {
     this.handleDisplay = this.handleDisplay.bind(this);
   }
 
+  handleClick(event) {
+    const result = calculate(this.state, event.target.value);
+    this.setState(result);
+  }
+
   handleDisplay(event) {
-    console.log('called handleDisplay');
     this.setState({
       total: event.target.value,
     });
   }
 
-  handleClick(event) {
-    console.log('called handleClick');
-    const result = calculate(this.state, event.target.value);
-    this.setState(result);
-  }
-
   render() {
+    const currentState = this.state;
     return (
       <div className="calculator">
-        <div className="screen" onChange={this.handleDisplay}>{this.state.total}</div>
+        <div className="screen" onChange={this.handleDisplay}>{currentState.total || currentState.next || 0}</div>
         <div className="row">
-          <button type="button" className="gray" value="AC" onClock={this.handleClick}>AC</button>
-          <button type="button" className="gray" value="+/-" onClock={this.handleClick}>+/-</button>
-          <button type="button" className="gray" value="%" onClock={this.handleClick}>%</button>
-          <button type="button" className="orange" value="÷" onClock={this.handleClick}>&#247;</button>
+          <button type="button" className="gray" value="AC" onClick={this.handleClick}>AC</button>
+          <button type="button" className="gray" value="+/-" onClick={this.handleClick}>+/-</button>
+          <button type="button" className="gray" value="%" onClick={this.handleClick}>%</button>
+          <button type="button" className="orange" value="÷" onClick={this.handleClick}>&#247;</button>
         </div>
         <div className="row">
-          <button type="button" className="gray" value="7" onClock={this.handleClick}>7</button>
-          <button type="button" className="gray" value="8" onClock={this.handleClick}>8</button>
-          <button type="button" className="gray" value="9" onClock={this.handleClick}>9</button>
-          <button type="button" className="orange" value="x" onClock={this.handleClick}>x</button>
+          <button type="button" className="gray" value="7" onClick={this.handleClick}>7</button>
+          <button type="button" className="gray" value="8" onClick={this.handleClick}>8</button>
+          <button type="button" className="gray" value="9" onClick={this.handleClick}>9</button>
+          <button type="button" className="orange" value="x" onClick={this.handleClick}>x</button>
         </div>
         <div className="row">
-          <button type="button" className="gray" value="4" onClock={this.handleClick}>4</button>
-          <button type="button" className="gray" value="5" onClock={this.handleClick}>5</button>
-          <button type="button" className="gray" value="6" onClock={this.handleClick}>6</button>
-          <button type="button" className="orange" value="-" onClock={this.handleClick}>-</button>
+          <button type="button" className="gray" value="4" onClick={this.handleClick}>4</button>
+          <button type="button" className="gray" value="5" onClick={this.handleClick}>5</button>
+          <button type="button" className="gray" value="6" onClick={this.handleClick}>6</button>
+          <button type="button" className="orange" value="-" onClick={this.handleClick}>-</button>
         </div>
         <div className="row">
-          <button type="button" className="gray" value="1" onClock={this.handleClick}>1</button>
-          <button type="button" className="gray" value="2" onClock={this.handleClick}>2</button>
-          <button type="button" className="gray" value="3" onClock={this.handleClick}>3</button>
-          <button type="button" className="orange" value="+" onClock={this.handleClick}>+</button>
+          <button type="button" className="gray" value="1" onClick={this.handleClick}>1</button>
+          <button type="button" className="gray" value="2" onClick={this.handleClick}>2</button>
+          <button type="button" className="gray" value="3" onClick={this.handleClick}>3</button>
+          <button type="button" className="orange" value="+" onClick={this.handleClick}>+</button>
         </div>
         <div className="row">
-          <button type="button" className="zero gray" value="0" onClock={this.handleClick}>0</button>
-          <button type="button" className="gray" value="." onClock={this.handleClick}>.</button>
-          <button type="button" className="orange" value="=" onClock={this.handleClick}>=</button>
+          <button type="button" className="zero gray" value="0" onClick={this.handleClick}>0</button>
+          <button type="button" className="gray" value="." onClick={this.handleClick}>.</button>
+          <button type="button" className="orange" value="=" onClick={this.handleClick}>=</button>
         </div>
       </div>
     );
