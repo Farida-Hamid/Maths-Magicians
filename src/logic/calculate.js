4642486a249d776071d9492b306b9783a45d9c27
Farida-Hamid <faridaaddala@gmail.com>
2022-07-07 15:40:57 +0300

    Correct inputs of initial state

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -14,6 +14,7 @@ function isNumber(item) {
  *   operation:String  +, -, etc.
  */
 export default function calculate(obj, buttonName) {
+  console.log('calculating obj=', obj, 'buttonName=', buttonName);
   if (buttonName === 'AC') {
     return {
       total: null,
