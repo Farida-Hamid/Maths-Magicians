4aca146063438b85e52699d5aa62e8c9ec52aed0
Farida-Hamid <faridaaddala@gmail.com>
2022-07-12 16:42:27 +0300

    Add the skelaton for npm install react-router-dom --save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
 import Calculator from './components/calculator';
 
-const App = (() => <Calculator />
+const App = (() =>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
