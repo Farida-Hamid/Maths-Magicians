6db7a221a6f6507693ad2657a9b1493ebc69343b
Farida-Hamid <faridaaddala@gmail.com>
2022-07-14 15:16:28 +0300

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom';
+import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Math from './components/Math';
--- src/components/Math.js
+++ src/components/Math.js
@@ -2,10 +2,10 @@ import Calculator from './calculator';
 
 function Math() {
   return (
-    <>
+    <div className="calculatorPage">
       <h2>Let&apos;s do some Math!</h2>
       <Calculator />
-    </>
+    </div>
   );
 }
 
